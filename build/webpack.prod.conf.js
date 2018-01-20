f4f6e51c3c996b61273ab52d4b618af7e76e9684
Yu da Lawrence Chou <b02611002@ntu.edu.tw>
2018-01-20 22:42:09 +0800

    :wrench: Build to /docs instead of /dist
    x

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -53,7 +53,7 @@ const webpackConfig = merge(baseWebpackConfig, {
         safe: true
       }
     }),
-    // generate dist index.html with correct asset hash for caching.
+    // generate docs index.html with correct asset hash for caching.
     // you can customize output by editing /index.html
     // see https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
@@ -106,9 +106,9 @@ const webpackConfig = merge(baseWebpackConfig, {
     new SWPrecacheWebpackPlugin({
       cacheId: 'g0v-summit-2018',
       filename: 'service-worker.js',
-      staticFileGlobs: ['dist/**/*.{js,html,css}'],
+      staticFileGlobs: ['docs/**/*.{js,html,css}'],
       minify: true,
-      stripPrefix: 'dist/'
+      stripPrefix: 'docs/'
     })
   ]
 })
