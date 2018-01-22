74dad9fcbe3e113a4890611f41fb1a5d46c9c95b
Yu da Lawrence Chou <b02611002@ntu.edu.tw>
2018-01-22 23:44:45 +0800

    ✨ Replace <Hello> with <LandingPage>

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+
+    <section>(To be replace with Anymod editable section)</section>
+
+    <section>(To be replace with Anymod editable section)</section>
+
+    <section>(To be replace with Anymod editable section)</section>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'landingPage',
+    data () {
+      return {
+        foo: 'bar'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
