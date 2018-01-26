5fd929348a030275d865a7d7c0b7c6618df5a356
Yu da Lawrence Chou <b02611002@ntu.edu.tw>
2018-01-27 01:05:00 +0800

    ✨ Create <TW> and <EN>

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import TW from '@/components/TW'
+import EN from '@/components/EN'
 import router from './router'
 import store from '@/store'
 
 Vue.config.productionTip = false
+Vue.component('TW', TW)
+Vue.component('EN', EN)
 
 /* eslint-disable no-new */
 new Vue({
