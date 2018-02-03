9a36bd2b9178eb9271373d00d7445db0483555ad
Yu da Lawrence Chou <b02611002@ntu.edu.tw>
2018-02-03 23:53:28 +0800

    :art: Move LandingPage.vue to pug

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,45 +1,37 @@
-<template>
-  <div class="landing-page">
+<template lang="pug">
+  .landing-page
+    section
+      h2
+        TW 關於 g0v.tw 台灣零時政府社群
+        EN About g0v.tw
 
-    <section>
-      <h2><TW>關於 g0v.tw 台灣零時政府社群</TW><EN>About g0v.tw</EN></h2>
+      TW
+        h3 草根的、科技的、開放的公民參與
+        p g0v.tw 台灣零時政府社群從 2012 年 10 月開始，用數位世代的思維，以開源 (open source)方式號召群眾以網路世代的新模式協作，運用科技力量促進政府透明化。截至 2016 年 3 月已有 40+ 場黑客松、受邀演講 50+ 場、媒體報導 80+ 次、1000+ 遍佈三大洲的貢獻者，成果皆以自由軟體模式釋出。 g0v 社群草根的、科技的、開放的特色，開創了公民參與的新想像。
+        h3 官民協作 開放參與
+        p 三年多來 g0v 社群的開源群眾貢獻刺激了台灣開放政府、開放資料、公民參與、新媒體等領域的發展，直至今日仍是數位草根公民運動的重要動力。2015 年，台灣在英國「開放知識基金會」（Open Knowledge Foundation）的開放資料評比中，從前一年度11名躍升成為全球第一名，正是包括 g0v 在內多個開放資料社群以及政府部門共同的協作努力。「開放透明」「公民參與」也逐漸成為大眾對政治運作的基礎要求。/         h3 從「寫程式改造社會」到「跨界公民參與」
+        p 社群於 2014 年 11 月舉辦 g0v Summit 零時政府高峰會（g0v Summit 2014），2015 年 8 月與世界銀行集團合作舉辦資料新聞實戰營（d|Bootcamp Taipei），皆是希望促成更多多元跨界協作的努力。g0v 社群以強勁的跨界動能拓展各種成果，在國際公民科技（civic tech）社群也受到矚目，並迅速在三年間成為全球最大開源開放政府社群。
 
-      <TW>
-        <h3>草根的、科技的、開放的公民參與</h3>
-        <p>g0v.tw 台灣零時政府社群從 2012 年 10 月開始，用數位世代的思維，以開源 (open source)方式號召群眾以網路世代的新模式協作，運用科技力量促進政府透明化。截至 2016 年 3 月已有 40+ 場黑客松、受邀演講 50+ 場、媒體報導 80+ 次、1000+ 遍佈三大洲的貢獻者，成果皆以自由軟體模式釋出。 g0v 社群草根的、科技的、開放的特色，開創了公民參與的新想像。</p>
-        <h3>官民協作 開放參與</h3>
-        <p>三年多來 g0v 社群的開源群眾貢獻刺激了台灣開放政府、開放資料、公民參與、新媒體等領域的發展，直至今日仍是數位草根公民運動的重要動力。2015 年，台灣在英國「開放知識基金會」（Open Knowledge Foundation）的開放資料評比中，從前一年度11名躍升成為全球第一名，正是包括 g0v 在內多個開放資料社群以及政府部門共同的協作努力。「開放透明」「公民參與」也逐漸成為大眾對政治運作的基礎要求。</p>
-        <h3>從「寫程式改造社會」到「跨界公民參與」</h3>
-        <p>社群於 2014 年 11 月舉辦 g0v Summit 零時政府高峰會（g0v Summit 2014），2015 年 8 月與世界銀行集團合作舉辦資料新聞實戰營（d|Bootcamp Taipei），皆是希望促成更多多元跨界協作的努力。g0v 社群以強勁的跨界動能拓展各種成果，在國際公民科技（civic tech）社群也受到矚目，並迅速在三年間成為全球最大開源開放政府社群。</p>
-      </TW>
+      EN 
+        p g0v.tw is a civic tech community in Taiwan since 2012 with deep open-source roots. With 1,000+ contributors through 40+ hackathons, it is recognized as one of the largest group in the global civic tech community. The community focuses on building tools for better information disclosure, engagement, and online democracy, advocating government transparency through the power of information technology.
+        p Through the years, the g0v community stimulated the progress of open government, open data, civic participation, and new media in Taiwan. Today the open community is still one of the main driving force for grass root civic movements, through interdisciplinary collaboration.
 
-      <EN>
-        <p>g0v.tw is a civic tech community in Taiwan since 2012 with deep open-source roots. With 1,000+ contributors through 40+ hackathons, it is recognized as one of the largest group in the global civic tech community. The community focuses on building tools for better information disclosure, engagement, and online democracy, advocating government transparency through the power of information technology.</p>
-        <p>Through the years, the g0v community stimulated the progress of open government, open data, civic participation, and new media in Taiwan. Today the open community is still one of the main driving force for grass root civic movements, through interdisciplinary collaboration.</p>
-      </EN>
-    </section>
-
-    <section>
-      <TW><i>(一些中文內容...)</i></TW>
-      <EN><i>(some english content)</i></EN>
-    </section>
-
-    <section>
-      ...
-    </section>
-
-  </div>
+    section 
+      TW 
+        i (一些中文內容...)
+      EN 
+        i (some english content)
 </template>
 
 <script>
-  export default {
-    name: 'landingPage',
-    data () {
-      return {
-        foo: 'bar'
-      }
+export default {
+  name: 'landingPage',
+  data () {
+    return {
+      foo: 'bar'
     }
   }
+}
 </script>
 
 <style scoped>
