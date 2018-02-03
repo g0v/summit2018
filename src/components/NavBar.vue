c9c3002cb9fe562445c994871591f2454ad47747
Yu da Lawrence Chou <b02611002@ntu.edu.tw>
2018-02-04 03:33:24 +0800

    :sparkles: Add NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,64 @@
+<template lang="pug">
+  .top-bar.stacked-for-medium
+    .top-bar-left
+      ul.dropdown.menu(data-dropdown-menu)
+        li.menu-text
+          TW 啥米零時政府
+          EN g0v summmit 2018
+    .top-bar-right
+      ul.menu
+        li
+          a(href="/schedule").disabled
+            TW 議程
+            EN Schedule
+        li
+          a(href="/speakers")
+            TW 講者
+            EN Speakers
+        li
+          a(href="/sponsors")
+            TW 贊助
+            EN Sponsors
+        li
+          a(href="/transport")
+            TW 交通
+            EN Transport
+        li
+          a(href="/live")
+            TW 直撥
+            EN Live
+        li
+          a(href="/staff")
+            TW 工作人員
+            EN Staff
+        li
+          a(href="/wiki")
+            TW 大會共筆
+            EN Wiki
+        li
+          a(@click="toggleLang")
+            span(v-if="lang === 'TW'")
+              b 中
+              span /En
+            span(v-else)
+              span 中/
+              b En
+        //- li
+        //-   input(type="search", placeholder="Search")
+        //- li
+        //-   button(type="button").button Search
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex'
+
+export default {
+  name: 'nav-bar',
+  computed: mapState(['lang']),
+  methods: mapMutations(['toggleLang'])
+}
+</script>
+
+<style scoped>
+
+</style>
