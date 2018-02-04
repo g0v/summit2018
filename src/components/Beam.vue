29eabe689fd7ec2c1f1eda697abf9467e0641758
Yu da Lawrence Chou <b02611002@ntu.edu.tw>
2018-02-04 22:17:11 +0800

    :construction: Add <Beam> component

---
+++ src/components/Beam.vue
@@ -0,0 +1,23 @@
+<template>
+  <g :transform="`translate(${x} ${y})`">
+    <path :stroke="colors[1]" :d="`M0 0 H3000`" :stroke-width="2*r"></path>
+    <path v-if="colors[2]" :stroke="colors[2]" :d="`M0 ${2*r/3} H3000`" :stroke-width="2*r/3"></path>
+    <circle :fill="colors[0]" :r="r"></circle>
+  </g>
+</template>
+
+<script>
+export default {
+  name: 'beam',
+  props: {
+    colors: { type: Array, default: () => ['#C55B3B', '#E3DDAB', '#DEB793'] },
+    r: { type: Number, default: 10 },
+    x: { type: Number, default: 10 },
+    y: { type: Number, default: 10 }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
