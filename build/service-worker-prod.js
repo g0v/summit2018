e17b360f22ab373bbfd3d49cc36bbd962404b48e
Yu da Lawrence Chou <b02611002@ntu.edu.tw>
2018-02-10 16:33:11 +0800

    🐛  Reload autometically if fetched page update

--- build/service-worker-prod.js
+++ build/service-worker-prod.js
@@ -35,6 +35,8 @@
                     // fresh content will have been added to the cache.
                     // It's the perfect time to display a "New content is
                     // available; please refresh." message in the page's interface.
+                    console.log('New content is avalible, the page will be reload.');
+                    location.reload();
                     break;
 
                   case 'redundant':
