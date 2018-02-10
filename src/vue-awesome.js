994a1d4a78427b5357d0ee8091c4cbb77668e933
Yu da Lawrence Chou <b02611002@ntu.edu.tw>
2018-02-10 14:24:25 +0800

    ✨ Setup vue-awesome and register <FaIcon>

---
+++ src/vue-awesome.js
@@ -0,0 +1,7 @@
+import FaIcon from 'vue-awesome/components/Icon.vue'
+import 'vue-awesome/icons/github'
+import 'vue-awesome/icons/twitter'
+import 'vue-awesome/icons/facebook'
+import 'vue-awesome/icons/slack'
+
+export default FaIcon
