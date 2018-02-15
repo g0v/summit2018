851033e32314a05452f990ca01c5de37936611e1
Yu da Lawrence Chou <b02611002@ntu.edu.tw>
2018-02-16 06:42:46 +0800

    ✨ Extract <MediaQuote>

---
+++ src/views/MediaQuote.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+
+.meida-quote
+  blockquote {{ report.quote }}
+    cite
+      a(:href="report.url") {{ report.title}}
+      span - {{ report.name }}
+
+</template>
+
+<script>
+import has from 'lodash/has'
+
+export default {
+  name: 'media-quote',
+  props: {
+    report: {
+      type: Object,
+      required: true,
+      validator: (item) =>
+        has(item, 'name') &&
+        has(item, 'title') &&
+        has(item, 'iconUrl') &&
+        has(item, 'url') &&
+        has(item, 'quote')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
