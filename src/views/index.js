d1693fb120875b518294657575ba01b354e4605c
Yu da Lawrence Chou <b02611002@ntu.edu.tw>
2018-03-10 04:17:09 +0800

    ✨新增頁尾贊助商區塊

--- src/views/index.js
+++ src/views/index.js
@@ -1,6 +1,7 @@
 export { default as NavBar } from './NavBar'
 export { default as TheFooter } from './TheFooter'
 export { default as SponsorUs } from './SponsorUs'
+export { default as SponsorSection } from './SponsorSection'
 export { default as TheTimeline } from './TheTimeline'
 export { default as SpeakerBrief } from './SpeakerBrief'
 export { default as MediaQuote } from './MediaQuote'
