ef555232ecbe57615053273de3ca9493f5fcd63b
Yu da Lawrence Chou <b02611002@ntu.edu.tw>
2018-03-31 14:24:17 +0800

    💄(/sponsors) Always use large sponsor icon

--- src/pages/SponsorPage.vue
+++ src/pages/SponsorPage.vue
@@ -85,13 +85,11 @@ export default {
      * 來自 Airtable 的贊助商資料
      */
     sponsorsByClass() {
-      const logoType = window.innerWidth > 640 ? 'large' : 'small'
-
       const sponsors = SponsorData.records
         .filter(record => record.fields.SHOW === true)
         .map(record => ({
           ...record.fields,
-          LOGO: record.fields.CROPPED_LOGO[0].thumbnails[logoType].url,
+          LOGO: record.fields.CROPPED_LOGO[0].thumbnails.large.url,
           id: record.id,
         }))
 
