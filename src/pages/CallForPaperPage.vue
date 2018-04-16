7deae8ccfdfab40a81965cda5ad56e3a220ddced
choznerol <lawrencechou1024@gmail.com>
2018-04-16 22:20:14 +0800

    ✨Add new page '/cfp'

---
+++ src/pages/CallForPaperPage.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  .call-for-paper-page Call for Paper page
+
+</template>
+
+<script>
+export default {
+  name: 'CallForPaperPage',
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
