3761a217699416cab3de418a2391709110d8ddf8
choznerol <lawrencechou1024@gmail.com>
2018-08-27 01:22:26 +0800

    🔥 Clean up Landing page style

--- src/pages/LandingPage.vue
+++ src/pages/LandingPage.vue
@@ -1,126 +1,114 @@
 <template lang="pug">
   #LandingPage
     HeroImage
-    #event-intro.grid-x.grid-padding-x.align-center.flex-child-shrink.mb-50
-      //- 我要贊助
-      section#cta.cell.text-center.mb-50
-        CtaButton(TW='一般票' EN='Registration' href="https://g0v-summit2018.kktix.cc/events/conf")
-        CtaButton(TW='貢獻者票' EN='Contributor Registration' href="https://g0v-summit2018.kktix.cc/events/contributor")
+    //- 我要贊助
+    section#cta.text-center.mb-50
+      CtaButton(TW='一般票' EN='Registration' href="https://g0v-summit2018.kktix.cc/events/conf")
+      CtaButton(TW='貢獻者票' EN='Contributor Registration' href="https://g0v-summit2018.kktix.cc/events/contributor")
+    #event-intro.grid-y.align-middle.mb-50
+      //- 社群簡介
+      section
+        TW
+          h1 社群簡介
+          p 自 2012 年以來，公民科技運動風起雲湧，以開放透明、公民參與為號召的 g0v 台灣零時政府社群號召了第一場活動，以「寫程式改造社會」為口號，從此開始公民科技之火開始延燒，至今未歇。
+          p 五年多來，在上千位參與者集體的貢獻下，透過一場場黑客松，打造出多項以開放資料、開放政府為精神的資訊平台，以縮短數位落差為初衷，降低公民參與門檻，使得議題審議可立基在具體事實，提升理性討論公共事務的意識。這場開放資料、開放政府運動，g0v 社群也打破了民間與政府對抗的傳統運動路線，開創出台灣公民社會與政府互動的新模式。
+        EN
+          h1 About g0v
+          p g0v (pronounced “gov zero”) is a decentralized, grass-roots civic tech community based in Taiwan. We advocate information transparency and build tech solutions to promote civic engagement.
+          p Built on the spirits of open source and activism, g0v aims to use technology in the interest of the public good. Our missions are to allow citizens easy access to public information and to empower them to make a change. Substituting the “o” with “0″ in gov, we want to use the power of digital technologies to reform current politics so as to envision a new future of democracy.
+          p Established in 2012, g0v has since hosted over 50 hackathons and two international summits. Thousands of participants from different backgrounds have contributed to hundreds of projects, making g0v one of the biggest civic tech communities around the world.
 
-      #paragraphs.cell.large-8.grid-y
+      //- 雙年會簡介
+      section
+        TW
+          h1 雙年會簡介
+          p g0v 國際雙年會兩年舉辦一次，關注開放政府、開源協作、公民參與等面向，是國際開放政府社群的焦點活動，過去兩屆吸引多達十九國講者投稿。雙年會全部由社群參與者志願籌辦，議程也秉持開放協作精神，全部議程皆有口譯、文字記錄和直播。議程除了大會籌辦，也開放完整時段的開放工作坊（Unconference）由參與者提案決定議程內容，以演講、討論、自由對話等形式創造新的協作空間。閉幕前自由報名的全場閃電講更是開源社群傳統，以開放分享的精神貫穿年會。
+          p
+            a(href="mailto:g0v-summit-partner-2018@googlegroups.com") 贊助大會
+            span &nbsp;&nbsp;|&nbsp;&nbsp;
+            a(href="mailto:g0v-summit-2018-pr@googlegroups.com") 媒體聯絡
+            span &nbsp;&nbsp;|&nbsp;&nbsp;
+            a(href="https://g0v.hackpad.tw/ep/pad/static/2mprMIpYMz9") 活動行為準則
+        EN
+          h1 About g0v summit
+          p The g0v summit is a biennial event which focuses on open government, open source collaboration and civic participation. It was established in 2014 by the g0v community. The past two summits have gathered speakers from 19 countries and more than 2,000 local and international participants. The summit is run by volunteers, and it itself is a huge collaborative project of the community. We aim to build an open environment for everyone. Volunteers will help write online shared notes, make livestreaming videos, and do realtime translation, all of these are released under open licenses. Besides formal sessions, we emphasize on “unconference”, which enables participants to host sessions and organize their own summit. As part of the tradition in open source events, the summit always ends with lightening talks when everyone can share their projects on the stage.
+          p
+            a(href="mailto:g0v-summit-partner-2018@googlegroups.com") Sponsor the Event
+            span &nbsp;&nbsp;|&nbsp;&nbsp;
+            a(href="mailto:g0v-summit-2018-pr@googlegroups.com") Media Contact
+            span &nbsp;&nbsp;|&nbsp;&nbsp;
+            a(href="https://g0v.hackpad.tw/ep/pad/static/2mprMIpYMz9") Code of Conduct
 
-        //- 社群簡介
-        section#intro.cell.card
-          TW.card-section
-            h3 社群簡介
-            p 自 2012 年以來，公民科技運動風起雲湧，以開放透明、公民參與為號召的 g0v 台灣零時政府社群號召了第一場活動，以「寫程式改造社會」為口號，從此開始公民科技之火開始延燒，至今未歇。
-            p 五年多來，在上千位參與者集體的貢獻下，透過一場場黑客松，打造出多項以開放資料、開放政府為精神的資訊平台，以縮短數位落差為初衷，降低公民參與門檻，使得議題審議可立基在具體事實，提升理性討論公共事務的意識。這場開放資料、開放政府運動，g0v 社群也打破了民間與政府對抗的傳統運動路線，開創出台灣公民社會與政府互動的新模式。
-          EN.card-section
-            h3 About g0v
-            p g0v (pronounced “gov zero”) is a decentralized, grass-roots civic tech community based in Taiwan. We advocate information transparency and build tech solutions to promote civic engagement.
-            p Built on the spirits of open source and activism, g0v aims to use technology in the interest of the public good. Our missions are to allow citizens easy access to public information and to empower them to make a change. Substituting the “o” with “0″ in gov, we want to use the power of digital technologies to reform current politics so as to envision a new future of democracy.
-            p Established in 2012, g0v has since hosted over 50 hackathons and two international summits. Thousands of participants from different backgrounds have contributed to hundreds of projects, making g0v one of the biggest civic tech communities around the world.
+    #timeline
+      section Timeline
 
-        //- 雙年會簡介
-        section#intro.cell.card
-          TW.card-section
-            h3 雙年會簡介
-            p g0v 國際雙年會兩年舉辦一次，關注開放政府、開源協作、公民參與等面向，是國際開放政府社群的焦點活動，過去兩屆吸引多達十九國講者投稿。雙年會全部由社群參與者志願籌辦，議程也秉持開放協作精神，全部議程皆有口譯、文字記錄和直播。議程除了大會籌辦，也開放完整時段的開放工作坊（Unconference）由參與者提案決定議程內容，以演講、討論、自由對話等形式創造新的協作空間。閉幕前自由報名的全場閃電講更是開源社群傳統，以開放分享的精神貫穿年會。
-            p
-              a(href="mailto:g0v-summit-partner-2018@googlegroups.com") 贊助大會
-              span &nbsp;&nbsp;|&nbsp;&nbsp;
-              a(href="mailto:g0v-summit-2018-pr@googlegroups.com") 媒體聯絡
-              span &nbsp;&nbsp;|&nbsp;&nbsp;
-              a(href="https://g0v.hackpad.tw/ep/pad/static/2mprMIpYMz9") 活動行為準則
-          EN.card-section
-            h3 About g0v summit
-            p The g0v summit is a biennial event which focuses on open government, open source collaboration and civic participation. It was established in 2014 by the g0v community. The past two summits have gathered speakers from 19 countries and more than 2,000 local and international participants. The summit is run by volunteers, and it itself is a huge collaborative project of the community. We aim to build an open environment for everyone. Volunteers will help write online shared notes, make livestreaming videos, and do realtime translation, all of these are released under open licenses. Besides formal sessions, we emphasize on “unconference”, which enables participants to host sessions and organize their own summit. As part of the tradition in open source events, the summit always ends with lightening talks when everyone can share their projects on the stage.
-            p
-              a(href="mailto:g0v-summit-partner-2018@googlegroups.com") Sponsor the Event
-              span &nbsp;&nbsp;|&nbsp;&nbsp;
-              a(href="mailto:g0v-summit-2018-pr@googlegroups.com") Media Contact
-              span &nbsp;&nbsp;|&nbsp;&nbsp;
-              a(href="https://g0v.hackpad.tw/ep/pad/static/2mprMIpYMz9") Code of Conduct
+    #summit-highlight.grid-y.align-middle.mb-50
 
-      //- 時程
-      section#timelime.cell.large-4
-        TheTimeline(:items="timelineItems")
-
-      //- 贊助年會
-      //- section#sponsor-us.cell.medium-8
-      //-   ScrollToTack(tackClass="tack-sponsor-us")
-      //-     SponsorUs
-
-    #summit-highlight.grid-x.grid-padding-x.align-center
-      //- Summit hightlight
-      section#hightlights.cell
-        //- 重點講者
-        h3
+      section#keynotes
+        h1
           TW 2018 Keynote
           EN 2018 Keynote
-        #keynotes.grid-x.align-justify.grid-margin-x.mb-50
+        .grid-x.align-justify.grid-margin-x.mb-50
           template(v-for="keynote in keynotes")
             KeynoteBrief(:keynote="keynote").cell.large-6.medium-6.flex-child-shrink
-        h3
+
+      section#past-speakers
+        h1
           TW 歷年講者
           EN Past Speakers
-        #speakers.grid-x.align-justify.grid-margin-x.mb-50
+        .grid-x.align-justify.grid-margin-x.mb-50
           template(v-for="speaker in speakers")
             SpeakerBrief(:speaker="speaker").cell.large-4.medium-6.flex-child-shrink
 
-        .grid-x.grid-margin-x
-          //- 媒體報導
-          .cell.large-4
-            h3
-              TW 媒體報導
-              EN Media Coverage
-            #reports
-              .media-object(v-for="report in reports").stack-for-small
-                .media-object-section
-                  MediaQuote(:report="report")
+      section#media-coverage
+        h1
+          TW 媒體報導
+          EN Media Coverage
+        .grid-x.align-justify.grid-margin-x.mb-50
+          .media-object(v-for="report in reports").large-4.stack-for-small
+            .media-object-section
+              MediaQuote(:report="report")
 
-          //- 歷年活動
-          .cell.large-8
-            h3
-              TW 歷年活動
-              EN Past Events
-            #previous-events.grid-x
-              .card
-                .grid-x
-                  .cell.card-section.medium-7.small-order-2
-                    a(href="http://summit.g0v.tw/2016/", target="_blank")
-                      h5
-                        TW 2016 拆後重建
-                        EN 2016 g0v summit
-                    p
-                      TW 2016 年則以「拆政府原地重建」作為主題。時值新舊政府交接，台灣在開放資料評比中排名世界第一，多位社群參與者也進入政府擔任顧問、幕僚等職，在拆解傳統政治模式之後，討論如何以科技建立開放的公私協作新模式。這次有超過千位參與者，與來自 17 國的全球公民科技代表，分享歐洲的新興政治經驗、亞洲推展數位科技到傳統公民團體的經驗、各國數位進入政府搭建溝通橋樑的改造經驗等。
-                      EN The 2016 summit centered on the theme of “reconstruction”. 2016 is a year that Taiwan was ranked No.1 in Open Data Index, that Taiwan elected a new president, and that many g0v participants joined the government to “reconstruct” the system from inside. This summit invited participants to share and reflect on various efforts of “reconstruction”, ranging from how to build new systems for civic tech, how to practice liquid democracy in Europe, how to empower NGOs in Asia, and how to promote public-private partnership.
-                  .cell.card-section.medium-5.small-order-1
-                    a(href="http://summit.g0v.tw/2016/", target="_blank")
-                      img(src="../assets/LandingPage/past-events/summit2016.png").thumbnail
+      section#flickr
+        h1 g0v Flickr
+        .card: .card-section
+          <a data-flickr-embed="true"  href="https://www.flickr.com/photos/g0v" title="summit-2018-carousel">
+            <img src="https://farm8.staticflickr.com/7588/26460319244_1c602283e3_h.jpg" width="1600" height="709" alt="summit-2018-carousel">
+          </a>
 
-              .card
-                .grid-x
-                  .cell.card-section.medium-6
-                    a(href="http://summit.g0v.tw/2014/", target="_blank")
-                      img(src="../assets/LandingPage/past-events/summit2014.png").thumbnail
-                  .cell.card-section.medium-6
-                    a(href="http://summit.g0v.tw/2014/", target="_blank")
-                      h5
-                        TW 2014 啥米零時政府
-                        EN 2014 g0v summit
-                    p
-                      TW 2014 年的首次年會緊接在 318 公民運動之後，聚焦開放國會、參與式民主、網路協作的社會運動。有來自八國長期關注開放政府運動的國際講者，以及國內政府公部門、NGO、媒體、資訊好手，並與中央研究院社會學研究所二十週年慶「跨世紀學運論壇」合辦，促進跨界交流。
-                      EN The 2014 summit took place right after the Sunflower Movement, in which digital tools boosted hundreds of thousands of protestors to take the streets. This summit focused on open parliament, participatory democracy and online collaboration in social movements. It was also the first attempt to bring cross-displinary discussion among international civic tech hackers, open source communities, NGOs, media, academic and public servants.
+        //- 歷年活動（隱藏）
+        //- .cell.large-8
+        //-   h1
+        //-     TW 歷年活動
+        //-     EN Past Events
+        //-   #previous-events.grid-x
+        //-     .card
+        //-       .grid-x
+        //-         .cell.card-section.medium-7.small-order-2
+        //-           a(href="http://summit.g0v.tw/2016/", target="_blank")
+        //-             h5
+        //-               TW 2016 拆後重建
+        //-               EN 2016 g0v summit
+        //-           p
+        //-             TW 2016 年則以「拆政府原地重建」作為主題。時值新舊政府交接，台灣在開放資料評比中排名世界第一，多位社群參與者也進入政府擔任顧問、幕僚等職，在拆解傳統政治模式之後，討論如何以科技建立開放的公私協作新模式。這次有超過千位參與者，與來自 17 國的全球公民科技代表，分享歐洲的新興政治經驗、亞洲推展數位科技到傳統公民團體的經驗、各國數位進入政府搭建溝通橋樑的改造經驗等。
+        //-             EN The 2016 summit centered on the theme of “reconstruction”. 2016 is a year that Taiwan was ranked No.1 in Open Data Index, that Taiwan elected a new president, and that many g0v participants joined the government to “reconstruct” the system from inside. This summit invited participants to share and reflect on various efforts of “reconstruction”, ranging from how to build new systems for civic tech, how to practice liquid democracy in Europe, how to empower NGOs in Asia, and how to promote public-private partnership.
+        //-         .cell.card-section.medium-5.small-order-1
+        //-           a(href="http://summit.g0v.tw/2016/", target="_blank")
+        //-             img(src="../assets/LandingPage/past-events/summit2016.png").thumbnail
 
-              //- Flickr 內嵌
-              .card
-                .card-section
-                  h5: a(href="https://www.flickr.com/photos/g0v/albums/with/72157692801078814" target="_blank") g0v Flickr
-                //- .card-section
-                <a data-flickr-embed="true" data-header="false" data-footer="false"  href="https://www.flickr.com/photos/g0v/albums/72157692801078814" title="g0v flickr">
-                  <img alt="g0v flickr" src="https://farm8.staticflickr.com/7588/26460319244_975d4dd47e_c.jpg" width="100%" height="auto">
-                </a>
+        //-     .card
+        //-       .grid-x
+        //-         .cell.card-section.medium-6
+        //-           a(href="http://summit.g0v.tw/2014/", target="_blank")
+        //-             img(src="../assets/LandingPage/past-events/summit2014.png").thumbnail
+        //-         .cell.card-section.medium-6
+        //-           a(href="http://summit.g0v.tw/2014/", target="_blank")
+        //-             h5
+        //-               TW 2014 啥米零時政府
+        //-               EN 2014 g0v summit
+        //-           p
+        //-             TW 2014 年的首次年會緊接在 318 公民運動之後，聚焦開放國會、參與式民主、網路協作的社會運動。有來自八國長期關注開放政府運動的國際講者，以及國內政府公部門、NGO、媒體、資訊好手，並與中央研究院社會學研究所二十週年慶「跨世紀學運論壇」合辦，促進跨界交流。
+        //-             EN The 2014 summit took place right after the Sunflower Movement, in which digital tools boosted hundreds of thousands of protestors to take the streets. This summit focused on open parliament, participatory democracy and online collaboration in social movements. It was also the first attempt to bring cross-displinary discussion among international civic tech hackers, open source communities, NGOs, media, academic and public servants.
 
 </template>
 
@@ -198,50 +186,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-// #LandingPage {
-//   #event-intro, #summit-highlight {
-//     max-width: 1080px;
-//     margin: auto;
-//   }
-// }
-
-#event-intro {
-  #cta {
-    position: relative;
-  }
-  #intro {
-  }
-  #sponsor-us {
-    padding-top: 100px;
-    padding-bottom: 50px;
-  }
-}
-
-#summit-highlight {
-  #hightlights {
-  }
-  #speakers {
-  }
-  #reports {
-  }
-  #previous-events {
-  }
-}
-</style>
-
-<style lang="scss">
-.tack-sponsor-us {
-  @include breakpoint(medium) {
-    opacity: 0.93;
-    font-size: 0.75em;
-    position: fixed;
-    z-index: 1;
-    right: 20px;
-    bottom: 40px;
-    width: 320px;
-    .close-button {
-      display: block !important;
-    }
-  }
+#timeline {
+  background-color: #e7e7e7;
 }
 </style>
