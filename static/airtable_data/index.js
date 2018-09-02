7a14b009eb9b5eb03e608c3aafd6aaaa020084ab
choznerol <lawrencechou1024@gmail.com>
2018-09-02 20:57:17 +0800

    🔥 Remove extractField()

--- static/airtable_data/index.js
+++ static/airtable_data/index.js
@@ -5,11 +5,6 @@ import SPEAKERS from './SPEAKERS.json'
 import SCHEDULE from './SCHEDULE.json'
 import STAFF from './STAFF.json'
 
-// 展開 Airtable 回傳的 json 中的 `fields` 欄位
-export const extractFields = record => assign({ id: record.id }, record.fields)
-
-export const extractAllFields = records => records.map(extractFields)
-
 /**
  * @param {Object} record Airtable response 的單個 json object
  * @param {String} path record 中要 populate 的 Object key 位置
@@ -32,16 +27,6 @@ export const populateRecord = (record, path, linkedRecords) => {
 export const populateRecords = (records, path, linkedRecords) =>
   records.map(record => populateRecord(record, path, linkedRecords))
 
-export const POPULATED_SPEAKERS = populateRecords(
-  extractAllFields(SPEAKERS.records),
-  'SCHEDULE',
-  extractAllFields(SCHEDULE.records)
-)
+export const POPULATED_SPEAKERS = populateRecords(SPEAKERS, 'SCHEDULE', SCHEDULE)
 
-export const POPULATED_SCHEDULE = populateRecords(
-  extractAllFields(SCHEDULE.records),
-  'SPEAKER',
-  extractAllFields(SPEAKERS.records)
-)
-
-export const POPULATED_STAFF = extractAllFields(STAFF.records)
+export const POPULATED_SCHEDULE = populateRecords(SCHEDULE, 'SPEAKER', SPEAKERS)
